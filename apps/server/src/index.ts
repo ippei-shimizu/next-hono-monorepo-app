ac63af007e2e49e5d5624b3ed59fbe02e404261a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-03-28 00:52:58 +0900

    Fix: フロント側のclientでバックエンドの型をimportできていなかったため

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,25 +3,19 @@ import { Env } from "./db/client";
 import { cors } from "hono/cors";
 import { postsRouter } from "./routes/posts";
 
-const app = new Hono<{ Bindings: Env }>();
-
-app.use(
-  "/*",
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    exposeHeaders: ["Content-Type"],
-    maxAge: 864_000,
-    credentials: true,
-  }),
-);
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.route("/api/posts", postsRouter);
+const app = new Hono<{ Bindings: Env }>()
+  .use(
+    "/*",
+    cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      exposeHeaders: ["Content-Type"],
+      maxAge: 864_000,
+      credentials: true,
+    }),
+  )
+  .route("/api/posts", postsRouter);
 
 export default app;
 export type AppType = typeof app;
--- apps/server/src/routes/posts.ts
+++ apps/server/src/routes/posts.ts
@@ -1,20 +1,37 @@
 import { Hono } from "hono";
 import { createClient, Env } from "../db/client";
 import { zValidator } from "@hono/zod-validator";
-import { getPostsSchema } from "../schemas/posts";
+import { createPostSchema, getPostsSchema } from "../schemas/posts";
 import { posts } from "../db/schema";
 
-export const postsRouter = new Hono<{ Bindings: Env }>();
+export const postsRouter = new Hono<{ Bindings: Env }>()
+  .get("/", zValidator("query", getPostsSchema), async (c) => {
+    const { limit, offset } = c.req.valid("query");
+    const db = createClient({
+      SUPABASE_DATABASE_URL: c.env.SUPABASE_DATABASE_URL,
+    });
+    const allPosts = await db.select().from(posts).limit(limit).offset(offset);
 
-postsRouter.get("/", zValidator("query", getPostsSchema), async (c) => {
-  const { limit, offset } = c.req.valid("query");
-  const db = createClient({
-    SUPABASE_DATABASE_URL: c.env.SUPABASE_DATABASE_URL,
-  });
-  const allPosts = await db.select().from(posts).limit(limit).offset(offset);
+    return c.json({
+      success: true,
+      data: allPosts,
+    });
+  })
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const postData = c.req.valid("json");
+    const db = createClient({
+      SUPABASE_DATABASE_URL: c.env.SUPABASE_DATABASE_URL,
+    });
+    const newPost = await db
+      .insert(posts)
+      .values({
+        title: postData.title,
+        body: postData.body,
+      })
+      .returning();
 
-  return c.json({
-    success: true,
-    data: allPosts,
+    return c.json({
+      success: true,
+      data: newPost,
+    });
   });
-});
