72752e86c016828eab23270bc39d55b4bdb6ed1f
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-03-28 01:08:10 +0900

    Add: post一覧データ取得、一覧画面作成

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div></div>;
+import PostLists from "@/app/posts/_components/postLists";
+import { getPosts } from "@/services/getPosts";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const posts = await getPosts("10", "0");
+
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostLists posts={posts.data} />
+      </Suspense>
+    </div>
+  );
 }
--- apps/server/src/types/post.ts
+++ apps/server/src/types/post.ts
@@ -8,3 +8,11 @@ import {
 export type CreatePostInput = z.infer<typeof createPostSchema>;
 export type GetPostsParams = z.infer<typeof getPostsSchema>;
 export type GetPostParams = z.infer<typeof getPostSchema>;
+
+export type Post = {
+  id: number;
+  title: string | null;
+  body: string | null;
+  created_at: string;
+  updated_at: string;
+};
